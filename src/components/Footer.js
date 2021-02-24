363073980f472165721cf8b0bfbf1de5d922931a
anthony-nagtalon <ant.nagtalon22@gmail.com>
2021-02-24 15:12:29 -0800

    Updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function Footer() {
     
     return (
         <footer className="mt-5">
-            <Container>
-                <Row>
+            <Container fluid={true}>
+                <Row className="border-top justify-content-between p-3">
                     <Col className="p-0" md={3} sm={12}>
                         Allen A Nagtalon
+                    </Col>
+                    <Col className="p-0 d-flex justify-content-end" md={4}>
+                        This site was made by Allen A Nagtalon.
                     </Col>
                 </Row>
             </Container>
