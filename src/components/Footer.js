0833a9dc04923e0d8dc0c64ec303ecde4aabe8e6
anthony-nagtalon <ant.nagtalon22@gmail.com>
2021-02-24 13:06:12 -0800

    Created footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function Footer() {
+    
+    return (
+        <footer className="mt-5">
+            <Container>
+                <Row>
+                    <Col className="p-0" md={3} sm={12}>
+                        Allen A Nagtalon
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+
+}
+
+export default Footer;
