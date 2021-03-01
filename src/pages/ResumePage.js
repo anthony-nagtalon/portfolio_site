1308ed59cc9b5f9630f5d878056ca672b04bf4df
anthony-nagtalon <ant.nagtalon22@gmail.com>
2021-03-01 13:09:46 -0800

    Added resume to ResumePage

--- src/pages/ResumePage.js
+++ src/pages/ResumePage.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+
+import resume from '../assets/pdfs/resume.pdf';
 
 function ResumePage(props) {
 
+    pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+    function removeTextLayerOffset() {
+        const textLayers = document.querySelectorAll(
+            '.react-pdf__Page__textContent'  
+        );
+        textLayers.forEach((layer) => {
+            const { style } = layer;
+            style.top = '0';
+            style.left = '0';
+            style.transform = '';
+        });
+    }
+
     return (
-        <p>ResumePage works!</p>
+        <div class="d-flex justify-content-center">
+            <Document file={resume}>
+                <Page pageNumber={1} onLoadSuccess={removeTextLayerOffset} />
+            </Document>
+        </div>
     );
 
 }
